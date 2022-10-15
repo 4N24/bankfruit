7f450ab9107f198b1b99f9fe9c2582d8ea469718
rjp1 <46408746+4N24@users.noreply.github.com>
2022-10-15 14:40:32 -0500

    added folders for ui components, constants etc

---
+++ MyCell/mycell/lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Colors.blueAccent; //'default' primary uicolor
+const kTextColor = Color(0xFF3C4046); // 'default' text color
+const kBackgroundColor = Color(0xFFF9F8FD); //'default' background
+
+const double kDefaultPadding = 20.0;
